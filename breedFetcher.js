f3d89e76dce5a4d382256f3871b7e0c7035806d4
ZakiyaAlfughi <zakiyaalfughi@gmail.com>
2021-12-03 23:45:19 +0000

    Add files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,17 @@
 const request = require('request');
 
-//1st argument URL && 2nd argument local file path
-const input = process.argv.slice(2);
+const fetchBreedDescription = function(input, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, "nosuchbreed");
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Requested breed is not found!!!!!!!!!!!!!");
-    return;
-  }
-  if (error) {
-    console.log("error", error);
-  }
-  console.log("-------------------------------");
-  console.log(data[0].description);
-});
-
+module.exports = { fetchBreedDescription };
